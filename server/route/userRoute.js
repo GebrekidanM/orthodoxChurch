4dcee16c2a65538ab02dcf4b996acbd280490a91
GebrekidanM <designerkidan@gmail.com>
2025-03-18 17:43:08 +0300

    samSite

--- server/route/userRoute.js
+++ server/route/userRoute.js
@@ -60,7 +60,7 @@ router.post("/login", async (req, res) => {
         res.cookie("token", token, {
             httpOnly: true,
             secure: true,
-            sameSite: "strict",
+            sameSite: "None",
             expires: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000),
         });
     
